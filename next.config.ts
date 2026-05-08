50ea3f19a02e8b424768b39c16d13ad1db28cb9f
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-05-09 06:39:28 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/backend/:path*',
-        destination: 'http://13.62.160.51:8080/:path*',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
